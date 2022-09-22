367f61d1110ce7d078569de9231766c656bdb236
doemily <111350013+doemily@users.noreply.github.com>
2022-09-22 13:42:16 -0700

    copy constructor notes

--- app/src/main/java/com/example/day3/Spot.java
+++ app/src/main/java/com/example/day3/Spot.java
@@ -7,22 +7,56 @@ import android.util.Log;
 import java.util.Random;
 
 public class Spot {
+    // private float cx;
+    // private float cy;
     private float radius;
-    private float cx;
-    private float cy;
+    private float centers[];
     private Paint color;
 
     public Spot() {
-        cx = 50;
-        cy = 50;
+        centers = new float[2];
+        centers[0] = 50;
+        // cx = 50;
+        // cy = 50;
         radius = 50;
         color = new Paint();
         color.setARGB(255, 255, 0, 0);
     }
 
+    /*
+        // a normal constructor looks like this:
+        Spot aSpot = new Spot();
+        // duplicate requires a COPY constructor
+        Spot duplicateSpot = new Spot(aSpot);
+        duplicateSpot.setRandomColor(); // only changes the color of dupSpot
+        Spot bSpot = aSpot; // this gives a second reference to the original aSpot, meanwhile COPY constructor makes a brand new Spot object
+        Spot.setRandomColor(); // change the color of aSpot (aka bSpot)
+        // a reference is like having the address
+    */
+
+    // copy constructor
+    public Spot(Spot other) {
+        // goal of a copy constructor is to copy all of the data from the other object into THIS object (this object is a NEW one)
+        // cx = other.cx;
+        // this.cy = other.cy; // could use this.cy or cy
+        // first create new container (array, arrayList, etc.)
+        // copy each item into new container
+        // !!!!!! count the news when creating a copy constructor (MIDTERM) !!!!!!
+        centers = new float[2]; // exact line from default constructor
+        for (int i = 0; i < centers.length; i++) {
+            // if og item is an object that was allocated anywhere with a NEW, this must also create a new object
+            centers[i] = other.centers[i]; // okay because this is a primitive type (not made with new)
+        }
+        radius = other.radius;
+        // color = other.color; WRONG not primitive type, shallow copy, there for will match the color of the original spot.. if og spot changes to yellow, copySpot will also be yellow (WRONG)
+        color = new Paint(); // deep copy requires allocating NEW memory if the constructor allocated NEW memory
+        // OR color.setColor(other.color.getColor());
+
+    }
+
     public void draw(Canvas canvas) {
-        canvas.drawCircle(cx, cy, radius, color);
-        Log.d("Spot.draw", "drewAt "+cx+ " , "+cy);
+        canvas.drawCircle(centers[0], centers[1], radius, color);
+        // Log.d("Spot.draw", "drewAt "+cx+ " , "+cy);
     }
 
     public void setRadius(float r) {
@@ -30,8 +64,8 @@ public class Spot {
     }
 
     public void setCenters(float x, float y) {
-        cx = x;
-        cy = y;
+        centers[0] = x;
+        centers[1] = y;
     }
 
     public void setRandomColor() {
